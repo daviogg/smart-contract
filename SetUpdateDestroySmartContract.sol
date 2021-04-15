1507587088efc5cdddb692b5de9436ad58632b1c
root <root@CWDZ2FN53.gruppo.reti.it>
2021-04-15 10:05:33 +0200

    Add two sc for tests

---
+++ SetUpdateDestroySmartContract.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.1;
+
+contract StartStopUpdateExample {
+    
+    address public owner;
+    bool public paused;
+
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    function sendMoney() public payable {
+
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        require(owner == msg.sender, "Fuck you");
+        require(paused == false, "Contract Paused");
+        _to.transfer(address(this).balance);
+    }
+    
+    function setPaused(bool _paused) public{
+        require(owner == msg.sender, "Fuck you");
+        paused = _paused;
+    }
+    
+    function destroySmartContract(address payable _to) public {
+        require(msg.sender == owner, "Fuck you");
+        selfdestruct(_to);
+    }
+    
+}
