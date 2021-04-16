0d5bbc0af06960a8e8007f48ef1ea2d2c814f761
Davide Oggioni (Reti) <daviogg@hotmail.it>
2021-04-16 18:03:37 +0200

    Exception handling

---
+++ contracts/Exception.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract ExceptionExample {
+    
+    mapping(address => uint64) public balanceReceived;
+    
+    function receiveMoney() public payable {
+        assert(balanceReceived[msg.sender] + uint64(msg.value) >= balanceReceived[msg.sender]);
+        balanceReceived[msg.sender] += uint64(msg.value);
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(_amount <= balanceReceived[msg.sender], "not enough funds.");
+        assert(balanceReceived[msg.sender] >= balanceReceived[msg.sender] - _amount);
+
+        balanceReceived[msg.sender] -= uint64(_amount);
+        _to.transfer(_amount);
+    }
+}
